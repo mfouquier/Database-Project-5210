93582adf3ec94bd6c4a8eaa3ed152ec73edc4670
Matthew Fouquier <mfouquier1@gmail.com>
2021-11-12 14:12:13 -0500

    add ejs, css files, and connect to db

---
+++ app.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var mysql = require('mysql');
+var bodyParser = require("body-parser");
+var app = express();
+
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(__dirname + "/public"));
+
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '12qwaszx!@QWASZX',
+    database: 'photoDojo'
+});
+
+app.get("/", function (req, res) {
+    // Find count of users in DB
+    var q = "SELECT COUNT(*) AS count FROM users";
+    connection.query(q, function (err, results) {
+        if (err) throw err;
+        var count = results[0].count;
+        res.render("home", { count: count });
+    });
+});
+
+app.post("/register", function (req, res) {
+    var username = {
+        username: req.body.username
+    };
+    connection.query('INSERT INTO users SET ?', username, function (err, result) {
+        if (err) throw err;
+        res.redirect("/");
+    });
+});
+
+app.listen(8080, function () {
+    console.log("Server running on 8080!");
+});
