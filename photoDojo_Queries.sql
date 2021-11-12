05d26bfaf9717ff5ba5190732cc442973181586d
Matthew Fouquier <mfouquier1@gmail.com>
2021-11-12 14:56:42 -0500

    add additional queries

--- photoDojo_Queries.sql
+++ photoDojo_Queries.sql
@@ -1,4 +1,5 @@
--- Query 1 -> 5 oldest users
+-- Query 1 -> 5 newest / oldest users
+select username, created_at from users order by created_at limit 5;
 select username, created_at from users order by created_at desc limit 5;
 
 -- Query 2 -> What day do most registrations happen
@@ -11,7 +12,7 @@ select username, created_at from users order by created_at desc limit 5;
 
 -- Query 3 -> Average number of posts per user
  select 
- 	(select count(*) from photos) / (select count(*) from users) as avg
+ 	(select count(*) from photos) / (select count(*) from users) as avg;
 
 -- Query 4 -> Find users(bots) that have liked every photo
  select 
@@ -33,6 +34,18 @@ SELECT * FROM inactiveusers;
 SELECT * FROM likedphotos;
 -- Limit to Top 10 liked photos
 SELECT * FROM likedphotos LIMIT 10;
+
+-- Query 8 -> Number of Comments on each Photo
+ select image_url, COUNT(*) AS num_comments from comments
+ join photos on comments.photo_id = photos.id
+ group by photo_id
+ order by num_comments desc;
+
+-- Query 9 -> Number of followers per user
+select username, count(*) as num_followers from users u
+join follows f on u.id = f.followee_id
+group by f.followee_id
+order by num_followers desc;
 
 
  
