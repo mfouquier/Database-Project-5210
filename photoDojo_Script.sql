1e86c291f1abdf1b963039a93dde34bae5d55bd1
Matthew Fouquier <mfouquier1@gmail.com>
2021-11-15 07:14:21 -0500

    change to script

--- photoDojo_Script.sql
+++ photoDojo_Script.sql
@@ -69,18 +69,18 @@ CREATE TABLE unfollows (
 
 
 -- Triggers 
-DELIMITER $$
- 	create trigger prevent_self_follows
- 		before insert on follows for each row 
- 			begin 
- 					if new.follower_id = new.followee_id
- 				then
- 					signal sqlstate '45000'
- 					set message_text = 'You cannot follow yourself!';
- 				end if;
- 			end
- 			$$
-DELIMITER ;
+-- DELIMITER $$
+--  	create trigger prevent_self_follows
+--  		before insert on follows for each row 
+--  			begin 
+--  					if new.follower_id = new.followee_id
+--  				then
+--  					signal sqlstate '45000'
+--  					set message_text = 'You cannot follow yourself!';
+--  				end if;
+--  			end
+--  			$$
+-- DELIMITER ;
 
 DELIMITER $$
 create trigger capture_unfollows
